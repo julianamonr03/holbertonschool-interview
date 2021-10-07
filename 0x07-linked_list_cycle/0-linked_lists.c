8ee91990bcd26abad74fad48a08b200665e63e0e
julianamonr03 <julianamonroy03@hotmail.com>
2021-10-06 19:32:25 -0500

    All

--- 0x07-linked_list_cycle/0-linked_lists.c
+++ 0x07-linked_list_cycle/0-linked_lists.c
@@ -3,61 +3,30 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_cycle - function in C that checks if a singly linked list has
+ *  a cycle in it
+ * @list: head of singly linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-size_t print_listint(const listint_t *h)
+int check_cycle(listint_t *list)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	listint_t *tortoise, *hare;
 
-    return (n);
-}
+	if (!list)
+		return (0);
 
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-    listint_t *new;
+	tortoise = list;
+	hare = list;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+		if (hare == tortoise)
+			return (1);
+	}
 
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	return (0);
 }
